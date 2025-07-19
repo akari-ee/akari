93cb2127d0752f2c4e3d5ff083a8044a81f91870
coggiee <zentechie7@gmail.com>
2025-07-19 16:51:32 +0900

    fix: createServerClient 함수의 매개변수를 선택적으로 변경하고, loader에서 토큰 없이 호출하도록 수정

--- app/lib/supabase-server.ts
+++ app/lib/supabase-server.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "~/types/types_db";
 
-export async function createServerClient(token: Promise<string | null>) {
+export async function createServerClient(token?: Promise<string | null>) {
   return createClient<Database>(
     process.env.VITE_SUPABASE_URL!,
     process.env.VITE_SUPABASE_KEY!,
--- app/routes/_feed._index.tsx
+++ app/routes/_feed._index.tsx
@@ -1,4 +1,4 @@
-import { prefetchPhotoList } from "~/service/photo";
+import { getPhotoList, prefetchPhotoList } from "~/service/photo";
 import type { Route } from "./+types/_feed._index";
 import PhotoList from "~/components/photo-list";
 // import { createServerClient } from "~/lib/supabase-server";
@@ -6,6 +6,7 @@ import { getAuth } from "@clerk/react-router/ssr.server";
 import { HydrationBoundary, QueryClient } from "@tanstack/react-query";
 import { Suspense } from "react";
 import { createServerClient } from "~/lib/supabase-server";
+import { Await, data } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -15,9 +16,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export async function loader(args: Route.LoaderArgs) {
-  const token = (await getAuth(args)).getToken();
-  const supabase = await createServerClient(token);
-
+  const supabase = await createServerClient();
   const state = await prefetchPhotoList(supabase);
 
   return { dehydratedState: state };
