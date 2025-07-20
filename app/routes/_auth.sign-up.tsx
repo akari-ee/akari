b71686fa263ac92b733058a7223d0555eb135647
coggiee <zentechie7@gmail.com>
2025-07-21 00:19:12 +0900

    feat: SignUp 컴포넌트에 unsafeMetadata 속성 추가하여 작가 여부 설정

--- app/routes/_auth.sign-up.tsx
+++ app/routes/_auth.sign-up.tsx
@@ -4,7 +4,11 @@ export default function SignUpPage() {
   return (
     <main className="flex flex-col items-center justify-center gap-4 py-4 md:py-6 w-full h-full">
       <h1>Sign up route</h1>
-      <SignUp />
+      <SignUp
+        unsafeMetadata={{
+          isCreator: false,
+        }}
+      />
     </main>
   );
 }
