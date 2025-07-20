dcfe967de0e1aaa2f3e48fcd045072296475840e
coggiee <zentechie7@gmail.com>
2025-07-21 00:17:22 +0900

    style: @clerk/localizations 및 @radix-ui/react-tooltip 패키지 추가, 새로운 아이콘 컴포넌트 및 커스텀 토스트 컴포넌트 구현

---
+++ app/components/shared/custom-toast.tsx
@@ -0,0 +1,51 @@
+import { CircleCheckIcon, CircleXIcon } from "lucide-react";
+
+interface CToastProps {
+  title?: string;
+  description?: string;
+}
+
+export function CToastSuccess({ title, description }: CToastProps) {
+  return (
+    <div className="bg-background text-foreground w-fit rounded-full border px-3 py-3 shadow-lg">
+      <div className="flex flex-col gap-2">
+        <div className="flex grow gap-3 items-center">
+          <CircleCheckIcon
+            className="shrink-0 text-emerald-500"
+            size={16}
+            strokeWidth={3}
+            aria-hidden="true"
+          />
+          {title && (
+            <div className="flex grow justify-between gap-12">
+              <p className="text-sm font-bold">{title}</p>
+            </div>
+          )}
+        </div>
+        {description && <div className="text-sm">{description}</div>}
+      </div>
+    </div>
+  );
+}
+export function CToastError({ title, description }: CToastProps) {
+  return (
+    <div className="bg-background text-foreground w-fit rounded-full border px-4 py-3 shadow-lg">
+      <div className="flex flex-col gap-2">
+        <div className="flex grow gap-3 items-center">
+          <CircleXIcon
+            className="shrink-0 text-rose-500"
+            size={16}
+            strokeWidth={3}
+            aria-hidden="true"
+          />
+          {title && (
+            <div className="flex grow justify-between gap-12">
+              <p className="text-sm font-bold">{title}</p>
+            </div>
+          )}
+        </div>
+        {description && <div className="text-sm">{description}</div>}
+      </div>
+    </div>
+  );
+}
