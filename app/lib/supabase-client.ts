610796a79824b82582adb96ab52a999a16e6dd3b
coggiee <zentechie7@gmail.com>
2025-07-21 00:18:40 +0900

    feat: 커스텀 토스트 컴포넌트 추가 및 쿼리 클라이언트의 오류 및 성공 처리 로직 개선, Supabase 클라이언트의 토큰 처리 방식 수정

--- app/lib/supabase-client.ts
+++ app/lib/supabase-client.ts
@@ -2,15 +2,13 @@ import { useAuth } from "@clerk/react-router";
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "~/types/types_db";
 
-export function createBrowserClient() {
-  const { getToken } = useAuth();
-
+export function createBrowserClient(token?: Promise<string | null>) {
   return createClient<Database>(
     import.meta.env.VITE_SUPABASE_URL!,
     import.meta.env.VITE_SUPABASE_KEY!,
     {
       async accessToken() {
-        return (await getToken()) ?? null;
+        return token ?? null;
       },
     }
   );
--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,8 @@ import {
 } from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
 import { toast } from "sonner";
+import { CToastError, CToastSuccess } from "./components/shared/custom-toast";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -18,17 +20,29 @@ export default function Providers({ children }: { children: React.ReactNode }) {
             refetchInterval: false,
             throwOnError: false,
           },
+          mutations: {
+            retry: 0,
+            throwOnError: false,
+            onError: (error, _, context) => {
+              toast.custom((t) => <CToastError title="에러 발생" />);
+            },
+            onSuccess: () => {
+              toast.custom((t) => <CToastSuccess title="성공" />);
+            },
+          },
         },
         queryCache: new QueryCache({
           onError: (error) => {
-            toast("Event has been created.")
+            toast("Event has been created.");
           },
         }),
       })
   );
   return (
     <ThemeProvider defaultTheme={"light"} attribute="class">
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
