660c737dd8b380c50c979728a410d55a0606ecbc
coggiee <zentechie7@gmail.com>
2025-07-25 01:20:02 +0900

    feat: Vite 설정에 서버 프록시 추가 및 SSR 외부 모듈 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,19 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    proxy: {
+      "/upload": {
+        target: "https://filmin-worker.zentechie7.workers.dev",
+        changeOrigin: true,
+        secure: true, // https일 때는 secure: true 권장
+      },
+    },
+  },
+  ssr: {
+    noExternal: [
+      'lucide-react',
+      '@phosphor-icons/react'
+    ]
+  }
 });
