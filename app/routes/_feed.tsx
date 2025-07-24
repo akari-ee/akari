da7fba8b5cc31673c38faa8c2c1cb7da6d5767a1
coggiee <zentechie7@gmail.com>
2025-07-25 01:48:32 +0900

    feat: 모바일 메뉴 컴포넌트 추가 및 레이아웃 개선

--- app/routes/_feed.tsx
+++ app/routes/_feed.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
+import MobileMenu from "~/components/shared/mobile-menu";
 import NavBar from "~/components/shared/nav-bar";
 import SearchBar from "~/components/shared/search-bar";
+import { cn } from "~/lib/utils";
 
 export default function FeedLayout() {
   return (
     <div className="relative">
       <NavBar />
-      <aside className="px-4 sticky top-16 z-10 py-3 bg-background border-b border-divider md:hidden">
+      <MobileMenu />
+      <aside className="px-4 sticky top-18 z-50 py-3 bg-background border-b border-divider md:hidden">
         <SearchBar />
       </aside>
       <main className="px-6 flex-grow">
