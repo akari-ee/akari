11a0853311f08ecf750f451083cceb6e36bd07ae
coggiee <zentechie7@gmail.com>
2025-07-26 19:19:46 +0900

    chore: zod 패키지 버전 변경

--- app/hooks/use-creator-form.ts
+++ app/hooks/use-creator-form.ts
@@ -1,6 +1,6 @@
 // useAuthorForm.ts
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { z } from "zod/v4";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { SOCIAL_FIELDS } from "~/types/social-fields";
 
